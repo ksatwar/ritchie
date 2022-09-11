18640ce7fe3e69ad11a8197c9e45753222b4d86e
Kabir Singh Waraich <kabir@pop-os.localdomain>
2022-09-11 20:53:07 +0530

    Added my soltuions for 1.16 and 1.17
    These have not been compiled and checked with strings however.

---
+++ Chapter 1/1.17:linelim80.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAXLINE 1000
+#define LIMIT 80
+/* maximum input line length */
+int fetchline(char line[], int maxline);
+int main()
+{
+int len;
+/* current line length */
+char line[MAXLINE];
+
+while ((len = fetchline(line, MAXLINE)) > 0)
+if (len > LIMIT ) 
+{
+printf("%s",line);
+}
+return 0;
+}
+/* fetchline: read a line into s, return length */
+int fetchline(char s[],int lim)
+{
+int c, i;
+
+for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+s[i] = c;
+if (c == '\n') {
+s[i] = c;
+++i;
+}
+s[i] = '\0';
+return i;
+}
